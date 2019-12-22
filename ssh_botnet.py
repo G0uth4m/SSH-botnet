91418eb461b6eca96d02c934f77bafbd4ff3abde
Goutham Ramesh <50041245+G0uth4m@users.noreply.github.com>
2019-12-22 22:22:25 +0530

    code beautified

--- ssh_botnet.py
+++ ssh_botnet.py
@@ -1,12 +1,73 @@
-from pexpect import pxssh
 import os
 import nmap
 from termcolor import colored
-import getpass
-from datetime import datetime,date
+from botnet import Botnet
+import optparse
 
-print("""
-Author : Goutham Ramesh
+def get_arguments():
+	parser = optparse.OptionParser()
+	parser.add_option("-u", "--username", dest="user", help="Specify common username")
+	parser.add_option("-p", "--password", dest="password", help="Specify common password")
+	parser.add_option("-i", "--interface", dest="interface", help="Network interface")
+
+	(options, arguments) = parser.parse_args()
+	if not options.user:
+		print "[-] Specify common username accross ssh servers\n"
+		print parser.print_help()
+		exit()
+
+	if not options.password:
+		print "[-] Specify common password accross ssh servers\n"
+		print parser.print_help()
+		exit()
+		
+	if not options.interface:
+		print "[-] Specify network interface\n"
+		print parser.print_help()
+		exit()
+
+	return options		
+
+def getSshServers(myip):
+	nm = nmap.PortScanner()
+
+	print "\n[*] Scanning network for ssh servers ..."
+	nm.scan(myip + '/24')
+	print "[+] Scan complete\n"
+
+	hosts = nm.all_hosts()
+	hosts.remove(myip)
+
+	if len(hosts) == 0:
+		print "[-] No live hosts other than you found on this network"
+		exit()
+
+	ssh_servers = {}
+	for i in hosts:
+		openPorts = list(nm[i]['tcp'].keys())
+		for j in openPorts:
+			if nm[i]['tcp'][j]['name'] == 'ssh':
+				por = j
+				ssh_servers[i] = j
+				break
+			por = -1
+
+	return ssh_servers
+
+def listSshServers(ssh_servers):
+	f2 = open('session.txt', 'w')
+
+	for i, j in ssh_servers.items():
+		print "Host : " + i + "\t\t" + "port : " + str(j)
+		f2.write(i + ":" + str(j) + '\n')
+	print '\n'
+
+	f2.close()
+
+def main():
+	options = get_arguments()
+	print("""
+Author : Goutham R
 GitHub : https://github.com/G0uth4m
 	
 	- This is a simple tool handy for linux administrators 
@@ -30,99 +91,32 @@ GitHub : https://github.com/G0uth4m
              |_____|                              
 """)
 
-f = open('logs.txt', 'a')
-f2 = open('session.txt', 'w')
-
-interface = raw_input('[*] Interface : ')
-user_name = raw_input('[*] Single username for all systems : ')
-password = raw_input('[*] Single password for all users : ')
-
-f2.write(interface + '\n' + user_name + '\n' + password + '\n')
-
-#myip = os.popen("ifconfig | grep -Eo 'inet (addr:)?([0-9]*\.){3}[0-9]*' | grep -Eo '([0-9]*\.){3}[0-9]*' | grep -v '127.0.0.1'").read().replace('\n', '')
-myip = os.popen("ifconfig " + interface + " | grep \"inet \" | awk \'{print $2}\'").read().replace("\n", "")
-class Client:
-	def __init__(self, host, user, password, por):
-		self.host = host
-		self.user = user
-		self.password = password
-		self.por  = por
-		self.session = self.connect()
-	def connect(self):
-		try:
-			s = pxssh.pxssh()
-			s.login(self.host, self.user, self.password, port = self.por)
-			return s
-		except Exception, e:
-			print e
-			print '[-] Error Connecting'
-			exit()
-	def send_command(self, cmd):
-		self.session.sendline(cmd)
-		self.session.prompt()
-		return self.session.before
-
-def botnetCommand(command):
-	f.write(" -> " + str(date.today().strftime("%B %d, %Y")) + " ( " + datetime.now().strftime("%H:%M:%S") + ' ) ' + '\n\n')
-	for client in botNet:
-		output = client.send_command(command)
-		print '[*] Output from ' + client.host
-		#print '[*] ssh service running on port : ' + str(client.por)
-		print '[+] ' + output
-		f.write('[*] Output from ' + client.host + '\n')
-		f.write('[+] ' + output + '\n')
-	f.write(100*'-' + '\n')
-def addClient(host, user, password, por):
-	if por != -1:
-		client = Client(host, user, password, por)
-		botNet.append(client)
-	else:
-		print '[-] ssh server not running on ' + host	
-
-botNet = []
-
-nm = nmap.PortScanner()
-#os.system("figlet ssh_botnet")
-print("\n")
-print "[*] Scanning network for ssh servers ..."
-nm.scan(myip + '/24')
-print "[+] Scan complete"
-hosts = nm.all_hosts()
-hosts.remove(myip)
-
-if len(hosts) == 0:
-	print "[-] No ssh servers found on the network"
-	exit()
-
-ssh_servers = []
-
-for i in hosts:
-	openPorts = list(nm[i]['tcp'].keys())
-	for j in openPorts:
-		if nm[i]['tcp'][j]['name'] == 'ssh':
-			por = j
-			ssh_servers.append([i,j])
-			break
-		por = -1	
-	addClient(i, user_name, password, por)
-
-print "\nRunning ssh servers : "
-
-for i in ssh_servers:
-	print "Host : " + i[0] + "\t\t" + "port : " + str(i[1])
-	f2.write(i[0] + ":" + str(i[1]) + '\n')
-print '\n'
-
-f2.close()	
-
-while True:
-	strr = colored('ssh@botnet:~$ ', 'red', None, ['bold'])
-	a = raw_input(strr)
-
-	if a == "exit()" or a == "exit":
-		f.close()
-		print("\n[*] History of commands stored in logs.txt")
-		break;
-	else:	
-		botnetCommand(a)
+	interface = options.interface
+	user = options.user
+	password = options.password
 	
+	myip = os.popen("ifconfig " + interface + " | grep \"inet \" | awk \'{print $2}\'").read().replace("\n", "")
+	ssh_servers = getSshServers(myip)
+	listSshServers(ssh_servers)
+
+	choice = raw_input("Continue adding bots to the botnet?[Y/n] ")
+	if(choice in ["n", "N", "no"]):
+		exit()
+
+	botnet = Botnet()
+	for i,j in ssh_servers.items():
+		botnet.addBot(i, user, password, j)
+
+	while True:
+		strr = colored('ssh@botnet:~$ ', 'red', None, ['bold'])
+		a = raw_input(strr)
+
+		if a == "exit()" or a == "exit":
+			botnet.f.close()
+			print("\n[*] History of commands stored in logs.txt")
+			break;
+		else:	
+			botnet.sendCommandsToBots(a)
+
+if __name__ == "__main__":
+	main()
